e8a098d07da7038baa513e030365af030d5a35ce
liu-nikki <108967384+liu-nikki@users.noreply.github.com>
2024-09-18 20:50:56 -0700

    Add github repo to TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link to="/Kanbas">Github</Link></li>
+        <li><a href="https://github.com/liu-nikki/kanbas-react-web-app" id="wd-github">GitHub Repository</a></li>
       </ul>
     );
   }
